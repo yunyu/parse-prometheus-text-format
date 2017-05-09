4cd10c9c33cf1cba1d89816eb3704a0031d730f5
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-09 13:09:56 -0400

    Don't write empty labels

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const splitLines = require('split-lines');
 
+const STATE_NAME = 0;
+const STATE_STARTOFLABELNAME = 1;
+const STATE_ENDOFNAME = 2;
+const STATE_VALUE = 3;
+const STATE_ENDOFLABELS = 4;
+const STATE_LABELNAME = 5;
+const STATE_LABELVALUEQUOTE = 6;
+const STATE_LABELVALUEEQUALS = 7;
+const STATE_LABELVALUE = 8;
+const STATE_LABELVALUESLASH = 9;
+const STATE_NEXTLABEL = 10;
+const ERR_MSG = 'Invalid line: ';
+
 var metricsFile = fs.readFileSync('metrics', 'utf8');
 console.time('parse');
 var convertedMetrics = parse(metricsFile);
 console.timeEnd('parse');
-console.log(JSON.stringify(convertedMetrics, null, 4));
+// console.log(JSON.stringify(convertedMetrics, null, 4));
 
 function parse(metrics) {
     var lines = splitLines(metrics);
@@ -18,10 +31,7 @@ function parse(metrics) {
 
     for (var i = 0; i < lines.length; i++) {
         var line = lines[i].trim();
-        var lineMetric = undefined;
-        var lineHelp = undefined;
-        var lineType = undefined;
-        var lineSample = undefined;
+        var lineMetric = undefined, lineHelp = undefined, lineType = undefined, lineSample = undefined;
         if (line.length == 0) {
             // ignore blank lines
         } else if (line.startsWith('# ')) {
@@ -119,25 +129,7 @@ function unescapeHelp(line) {
 
 function parseSampleLine(line) {
     // adapted from https://github.com/prometheus/client_python/blob/ce7f2978499dbd24e1028ef8966e50f374f51f5a/prometheus_client/parser.py#L48
-    const STATE_NAME = 0;
-    const STATE_STARTOFLABELNAME = 1;
-    const STATE_ENDOFNAME = 2;
-    const STATE_VALUE = 3;
-    const STATE_ENDOFLABELS = 4;
-    const STATE_LABELNAME = 5;
-    const STATE_LABELVALUEQUOTE = 6;
-    const STATE_LABELVALUEEQUALS = 7;
-    const STATE_LABELVALUE = 8;
-    const STATE_LABELVALUESLASH = 9;
-    const STATE_NEXTLABEL = 10;
-    const ERR_MSG = 'Invalid line: ';
-
-    var name = '';
-    var labelname = '';
-    var labelvalue = '';
-    var value = '';
-    var labels = {};
-
+    var name = '', labelname = '', labelvalue = '', value = '', labels = undefined;
     var state = STATE_NAME;
 
     for (var c = 0; c < line.length; c++) {
@@ -198,6 +190,9 @@ function parseSampleLine(line) {
             if (charAt == '\\') {
                 state = STATE_LABELVALUESLASH;
             } else if (charAt == '"') {
+                if (!labels) {
+                    labels = {};
+                }
                 labels[labelname] = labelvalue;
                 labelname = '';
                 labelvalue = '';
