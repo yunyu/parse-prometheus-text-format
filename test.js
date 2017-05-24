2893a3038cc06c449acf2b7dc4a910cdabc025ec
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-23 18:56:42 -0600

    Add bench

--- test.js
+++ test.js
@@ -2,4 +2,13 @@ const parsePrometheusTextFormat = require('./index.js');
 const fs = require('fs');
 
 var inputStr = fs.readFileSync('testinput.txt', 'utf8');
-console.log(JSON.stringify(parsePrometheusTextFormat(inputStr), null, 4));
+if (process.argv[2] && process.argv[2] == 'bench') {
+    var parsed;
+    console.time('parse');
+    for (var i = 0; i < 50000; i++) {
+        parsed = parsePrometheusTextFormat(inputStr);
+    }
+    console.timeEnd('parse');
+} else {
+    console.log(JSON.stringify(parsePrometheusTextFormat(inputStr), null, 4));
+}
