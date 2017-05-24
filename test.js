62c05c2f0cba5fe67b7e78f686affd9865a76d57
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-23 19:12:35 -0600

    Also benchmark JSON parse for comparison (results are in same order of magnitude)

--- test.js
+++ test.js
@@ -5,10 +5,16 @@ var inputStr = fs.readFileSync('testinput.txt', 'utf8');
 if (process.argv[2] && process.argv[2] == 'bench') {
     var parsed;
     console.time('parse');
-    for (var i = 0; i < 50000; i++) {
+    for (let i = 0; i < 50000; ++i) {
         parsed = parsePrometheusTextFormat(inputStr);
     }
     console.timeEnd('parse');
+    var stringified = JSON.stringify(parsed, null, 4); // Multiple lines for fair comparison
+    console.time('json-parse');
+    for (let i = 0; i < 50000; ++i) {
+        parsed = JSON.parse(stringified);
+    }
+    console.timeEnd('json-parse');
 } else {
     console.log(JSON.stringify(parsePrometheusTextFormat(inputStr), null, 4));
 }
